1ea0973f23ad062d8f2b35e24ce99a1164f71e02
Vladislav Veluga <vladislav805@yandex.com>
2018-08-20 20:46:29 +0300

    New build

--- build.sh
+++ build.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
-ndkpath=/home/vlad805/Android/Sdk/ndk-bundle/build/
+sdkpath=/home/user/Android/Sdk
+ndkpath=$sdkpath/ndk-bundle/build
 abi=armeabi-v7a
-nowdate=$(date +'%Y-%m-%d')
+nowdate=$(date +'%Y%m%d')
 apkname="Spirit3-$nowdate"
 
-export KEYSTORE_FILE="/home/vlad805/git/androidkeystore.jks"
+export KEYSTORE_FILE="/home/user/androidkeystore.jks"
 export KEYSTORE_PASS="myNadusik"
 export KEY_NAME="alias"
 export PROJ="sf"
 
-android update project --target android-21 --path . --name $PROJ
+$sdkpath/tools/android update project --target android-21 --path . --name $PROJ
 echo $ndkpath/ndk-build start
-time $ndkpath/ndk-build #2>&1 | grep "error:"
+time $ndkpath/ndk-build 2>&1 | grep "error:"
 
 mv libs/$abi/s2d libs/$abi/libs2d.so
 mv libs/$abi/ssd libs/$abi/libssd.so
 
 ant -q clean release
 
-jarsigner -storepass $KEYSTORE_PASS -sigalg MD5withRSA -digestalg SHA1 -keystore $KEYSTORE_FILE -signedjar bin/$apkname-unaligned.apk bin/$apkname-unsigned.apk $KEY_NAME
+jarsigner -storepass $KEYSTORE_PASS -sigalg MD5withRSA -digestalg SHA1 -keystore $KEYSTORE_FILE -signedjar bin/$PROJ-release-unaligned.apk bin/$PROJ-release-unsigned.apk $KEY_NAME
 
-zipalign -f 4 bin/$apkname-unaligned.apk bin/$apkname.apk
+zipalign -f 4 bin/$PROJ-release-unaligned.apk bin/$PROJ-release.apk
+mv bin/$PROJ-release.apk bin/$apkname.apk
+rm -rf bin/$PROJ-*
 
 #adb install -r bin/$PROJ-release.apk
 #adb shell am start -n fm.a2d.sf/fm.a2d.sf.MainActivity

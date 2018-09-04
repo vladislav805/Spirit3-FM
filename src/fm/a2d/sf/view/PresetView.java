95251012d1dec1e532a773de3acbde26b4ceea9b
Vladislav Veluga <vladislav805@yandex.com>
2018-09-05 00:55:14 +0300

    TEST preset updates

--- src/fm/a2d/sf/view/PresetView.java
+++ src/fm/a2d/sf/view/PresetView.java
@@ -1,8 +1,10 @@
 package fm.a2d.sf.view;
 
 import android.content.Context;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.PopupMenu;
 import fm.a2d.sf.R;
 
 import static android.util.TypedValue.COMPLEX_UNIT_DIP;
@@ -16,9 +18,14 @@ public class PresetView extends Button {
   private int mIndex;
   private String mFrequency;
 
+  private static final int MENU_REMOVE = 0x1e0e;
+  private static final int MENU_RENAME = 0x1eae;
+  private static final int MENU_CREATE = 0x1add;
+
   public PresetView(Context context) {
     super(context);
 
+    setMaxLines(1);
     setLayoutParams(new ViewGroup.LayoutParams((int) context.getResources().getDimension(R.dimen.preset_item_width), ViewGroup.LayoutParams.MATCH_PARENT));
 
     setHapticFeedbackEnabled(true);
@@ -41,8 +48,26 @@ public class PresetView extends Button {
 
   public PresetView setListeners(OnClickListener onClick, OnLongClickListener onLongClick) {
     setOnClickListener(onClick);
-    setOnLongClickListener(onLongClick);
+    setOnLongClickListener(new OnLongClickListener() {
+      @Override
+      public boolean onLongClick(View v) {
+        onMenuOpen();
+        return true;
+      }
+    });
     return this;
+  }
+
+  private void onMenuOpen() {
+    Context c = getContext();
+    PopupMenu popupMenu = new PopupMenu(c, this);
+    if (isEmpty()) {
+      popupMenu.getMenu().add(1, MENU_CREATE, 1, c.getString(R.string.popup_preset_create));
+    } else {
+      popupMenu.getMenu().add(1, MENU_RENAME, 1, c.getString(R.string.popup_preset_rename));
+      popupMenu.getMenu().add(1, MENU_REMOVE, 2, c.getString(R.string.popup_preset_remove));
+    }
+    popupMenu.show();
   }
 
   public int getIndex() {

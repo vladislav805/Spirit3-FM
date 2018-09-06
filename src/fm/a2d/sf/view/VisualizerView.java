6706e767726bdb7ab53a31152aa1835c996a0606
Vladislav Veluga <vladislav805@yandex.com>
2018-09-06 17:15:44 +0300

    TEST compress place on activity

--- src/fm/a2d/sf/view/VisualizerView.java
+++ src/fm/a2d/sf/view/VisualizerView.java
@@ -15,7 +15,6 @@ import android.view.View;
 import fm.a2d.sf.com_uti;
 
 public class VisualizerView extends View {
-  private static int m_obinits = 1;
   private boolean freq_avg_shared;
   private boolean freq_interp;
   private Paint freq_paint;
@@ -36,20 +35,19 @@ public class VisualizerView extends View {
   private Paint wave_paint;
   private int width;
 
-  class C00141 implements OnDataCaptureListener {
-    C00141() {
+  private OnDataCaptureListener mDataCaptureListener = new OnDataCaptureListener() {
+    @Override
+    public void onWaveFormDataCapture(Visualizer visualizer, byte[] waveform, int samplingRate) {
+      m_wave_data = waveform;
+      invalidate();
     }
 
-    public void onWaveFormDataCapture(Visualizer visualizer, byte[] bytes, int samplingRate) {
-      VisualizerView.this.m_wave_data = bytes;
-      VisualizerView.this.invalidate();
+    @Override
+    public void onFftDataCapture(Visualizer visualizer, byte[] fft, int samplingRate) {
+      m_freq_data = fft;
+      invalidate();
     }
-
-    public void onFftDataCapture(Visualizer visualizer, byte[] bytes, int samplingRate) {
-      VisualizerView.this.m_freq_data = bytes;
-      VisualizerView.this.invalidate();
-    }
-  }
+  };
 
   public VisualizerView(Context context) {
     this(context, null, 0);
@@ -71,37 +69,33 @@ public class VisualizerView extends View {
 
   public VisualizerView(Context context, AttributeSet attrs, int defStyle) {
     super(context, attrs);
-    this.m_wave_points = new float[16384];
-    this.m_freq_points = new float[16384];
-    this.m_audio_sessid = -3;
-    this.matr = new Matrix();
-    this.wave_interp = false;
-    this.wave_avg_shared = false;
-    this.freq_interp = true;
-    this.freq_avg_shared = true;
-    this.num_throwable = 0;
-    StringBuilder append = new StringBuilder().append("m_obinits: ");
-    int i = m_obinits;
-    m_obinits = i + 1;
-    com_uti.logd(append.append(i).toString());
+    m_wave_points = new float[16384];
+    m_freq_points = new float[16384];
+    m_audio_sessid = -3;
+    matr = new Matrix();
+    wave_interp = false;
+    wave_avg_shared = false;
+    freq_interp = true;
+    freq_avg_shared = true;
+    num_throwable = 0;
     try {
       com_uti.logd("context: " + context + "  attrs: " + attrs + "  defStyle: " + defStyle);
-      this.m_wave_data = null;
-      this.m_freq_data = null;
-      this.freq_paint = new Paint();
-      this.freq_paint.setStrokeWidth(0.0f);
-      this.freq_paint.setAntiAlias(false);
-      this.freq_paint.setColor(Color.argb(255, 181, 0, 255));
+      m_wave_data = null;
+      m_freq_data = null;
+      freq_paint = new Paint();
+      freq_paint.setStrokeWidth(0.0f);
+      freq_paint.setAntiAlias(false);
+      freq_paint.setColor(Color.argb(255, 181, 0, 255));
 
-      this.wave_paint = new Paint();
-      this.wave_paint.setStrokeWidth(0.0f);
-      this.wave_paint.setAntiAlias(false);
-      this.wave_paint.setColor(Color.argb(255, 0, 235, 165));
+      wave_paint = new Paint();
+      wave_paint.setStrokeWidth(0.0f);
+      wave_paint.setAntiAlias(false);
+      wave_paint.setColor(Color.argb(255, 0, 235, 165));
 
-      this.line_paint = new Paint();
-      this.line_paint.setStrokeWidth(0.0f);
-      this.line_paint.setAntiAlias(false);
-      this.line_paint.setColor(Color.argb(255, 255, 255, 192));
+      line_paint = new Paint();
+      line_paint.setStrokeWidth(0.0f);
+      line_paint.setAntiAlias(false);
+      line_paint.setColor(Color.argb(255, 255, 255, 192));
     } catch (Throwable e) {
       com_uti.loge("Throwable: " + e);
     }
@@ -109,20 +103,20 @@ public class VisualizerView extends View {
 
   public void vis_start(int audio_sessid) {
     try {
-      com_uti.logd("audio_sessid: " + audio_sessid + "  m_audio_sessid: " + this.m_audio_sessid + "  m_visualizer: " + this.m_visualizer);
-      this.m_audio_sessid = audio_sessid;
-      if (this.m_visualizer != null) {
+      com_uti.logd("audio_sessid: " + audio_sessid + "  m_audio_sessid: " + m_audio_sessid + "  m_visualizer: " + m_visualizer);
+      m_audio_sessid = audio_sessid;
+      if (m_visualizer != null) {
         com_uti.logd("m_visualizer active, stopping first");
         vis_stop();
       }
-      this.m_visualizer = new Visualizer(audio_sessid);
+      m_visualizer = new Visualizer(audio_sessid);
       int min = Visualizer.getCaptureSizeRange()[0];
       int max = Visualizer.getCaptureSizeRange()[1];
       com_uti.logd("min: " + min + "  max: " + max);
-      this.m_visualizer.setEnabled(false);
-      this.m_visualizer.setCaptureSize(max);
-      this.m_visualizer.setDataCaptureListener(new C00141(), Visualizer.getMaxCaptureRate() / 2, true, true);
-      this.m_visualizer.setEnabled(true);
+      m_visualizer.setEnabled(false);
+      m_visualizer.setCaptureSize(max);
+      m_visualizer.setDataCaptureListener(mDataCaptureListener, Visualizer.getMaxCaptureRate() / 2, true, true);
+      m_visualizer.setEnabled(true);
     } catch (Throwable e) {
       com_uti.loge("Throwable: " + e);
     }
@@ -130,13 +124,13 @@ public class VisualizerView extends View {
 
   public void vis_stop() {
     try {
-      com_uti.logd("m_audio_sessid: " + this.m_audio_sessid + "  m_visualizer: " + this.m_visualizer);
-      if (this.m_visualizer != null) {
-        this.m_visualizer.release();
+      com_uti.logd("m_audio_sessid: " + m_audio_sessid + "  m_visualizer: " + m_visualizer);
+      if (m_visualizer != null) {
+        m_visualizer.release();
       } else {
         com_uti.loge("Already stopped !!");
       }
-      this.m_visualizer = null;
+      m_visualizer = null;
     } catch (Throwable e) {
       com_uti.loge("Throwable: " + e);
     }
@@ -147,23 +141,23 @@ public class VisualizerView extends View {
       super.onDraw(canvas);
       width = getWidth();
       height = getHeight();
-      if (this.m_bitmap == null) {
-        this.m_bitmap = Bitmap.createBitmap(canvas.getWidth(), canvas.getHeight(), Config.ARGB_8888);
+      if (m_bitmap == null) {
+        m_bitmap = Bitmap.createBitmap(canvas.getWidth(), canvas.getHeight(), Config.ARGB_8888);
       }
-      if (this.m_canvas == null) {
-        this.m_canvas = new Canvas(this.m_bitmap);
+      if (m_canvas == null) {
+        m_canvas = new Canvas(m_bitmap);
       }
-      this.m_canvas.drawColor(0, Mode.CLEAR);
-      if (!(this.m_wave_data == null && this.m_freq_data == null)) {
+      m_canvas.drawColor(0, Mode.CLEAR);
+      if (!(m_wave_data == null && m_freq_data == null)) {
         scale_draw(canvas);
       }
-      if (this.m_wave_data != null) {
-        wave_render(this.m_canvas, this.m_wave_data);
+      if (m_wave_data != null) {
+        wave_render(m_canvas, m_wave_data);
       }
-      if (this.m_freq_data != null) {
-        freq_render(this.m_canvas, this.m_freq_data);
+      if (m_freq_data != null) {
+        freq_render(m_canvas, m_freq_data);
       }
-      canvas.drawBitmap(this.m_bitmap, this.matr, null);
+      canvas.drawBitmap(m_bitmap, matr, null);
     } catch (Throwable e) {
       com_uti.loge("Throwable: " + e);
     }
@@ -177,52 +171,52 @@ public class VisualizerView extends View {
       int pidx = 0;
       int cnt_y = 0;
       for (int idx = 0; idx < max_points; idx++) {
-        int y = (int) ((double) (((((byte) (wave_data[idx] + 128)) + 128) * this.height) / 512));
+        int y = (int) ((double) (((((byte) (wave_data[idx] + 128)) + 128) * height) / 512));
         if (y < 0) {
           y = 0;
         }
-        if (y >= this.height / 2) {
-          y = (this.height / 2) - 1;
+        if (y >= height / 2) {
+          y = (height / 2) - 1;
         }
-        int x = (this.width * idx) / max_points;
-        if (x >= this.width) {
-          x = this.width - 1;
+        int x = (width * idx) / max_points;
+        if (x >= width) {
+          x = width - 1;
         }
         if (last_x <= 0 || x != last_x) {
           cnt_y = 0;
           int next_x = last_x + 1;
           int loops = x - next_x;
-          if (!this.wave_interp || last_x < 0 || x <= next_x) {
-            if (x + 1 < this.width) {
-              this.m_wave_points[pidx] = (float) x;
-              this.m_wave_points[pidx + 2] = (float) (x + 1);
-              this.m_wave_points[pidx + 1] = (float) y;
-              this.m_wave_points[pidx + 3] = (float) y;
+          if (!wave_interp || last_x < 0 || x <= next_x) {
+            if (x + 1 < width) {
+              m_wave_points[pidx] = (float) x;
+              m_wave_points[pidx + 2] = (float) (x + 1);
+              m_wave_points[pidx + 1] = (float) y;
+              m_wave_points[pidx + 3] = (float) y;
               if (last_y >= 0) {
-                this.m_wave_points[pidx + 3] = (float) last_y;
+                m_wave_points[pidx + 3] = (float) last_y;
               }
               pidx += 4;
             }
           } else {
             for (int loop = 0; loop < loops - 1; loop++) {
-              this.m_wave_points[pidx] = (float) (next_x + loop);
-              this.m_wave_points[pidx + 2] = (float) ((next_x + loop) + 1);
-              this.m_wave_points[pidx + 1] = (float) y;
-              this.m_wave_points[pidx + 3] = (float) y;
+              m_wave_points[pidx] = (float) (next_x + loop);
+              m_wave_points[pidx + 2] = (float) ((next_x + loop) + 1);
+              m_wave_points[pidx + 1] = (float) y;
+              m_wave_points[pidx + 3] = (float) y;
               if (last_y >= 0) {
-                this.m_wave_points[pidx + 3] = (float) ((((last_y - y) * loop) / loops) + last_y);
+                m_wave_points[pidx + 3] = (float) ((((last_y - y) * loop) / loops) + last_y);
               }
               pidx += 4;
             }
           }
           last_x = x;
           last_y = y;
-        } else if (this.wave_avg_shared) {
+        } else if (wave_avg_shared) {
           cnt_y++;
-          this.m_wave_points[(pidx - 4) + 3] = (float) ((y + last_y) / cnt_y);
+          m_wave_points[(pidx - 4) + 3] = (float) ((y + last_y) / cnt_y);
         }
       }
-      canvas.drawLines(this.m_wave_points, this.wave_paint);
+      canvas.drawLines(m_wave_points, wave_paint);
     } catch (Throwable e) {
       com_uti.loge("Throwable: " + e);
     }
@@ -267,54 +261,54 @@ public class VisualizerView extends View {
         int x;
         byte real = freq_data[(i * 2)];
         byte imag = freq_data[(i * 2) + 1];
-        int y = (int) Math.round((((10.0d * amp_log((double) ((real * real) + (imag * imag)))) - 5.0d) * ((double) this.height)) / 40.0d);
+        int y = (int) Math.round((((10.0d * amp_log((double) ((real * real) + (imag * imag)))) - 5.0d) * ((double) height)) / 40.0d);
         if (y < 0) {
           y = 0;
         } else {
-          if (y >= this.height) {
-            y = this.height - 1;
+          if (y >= height) {
+            y = height - 1;
           }
         }
-        x = (int) ((((double) this.width) * freq_log((double) i)) / freq_log_points);
+        x = (int) ((((double) width) * freq_log((double) i)) / freq_log_points);
         if (x < 0) {
           x = 0;
         }
-        if (x >= this.width) {
-          x = this.width - 1;
+        if (x >= width) {
+          x = width - 1;
         }
         if (last_x <= 0 || x != last_x) {
           cnt_y = 0;
           int next_x = last_x + 1;
           int loops = x - next_x;
-          if (!this.freq_interp || last_x < 0 || x <= next_x) {
-            this.m_freq_points[pidx] = (float) x;
-            this.m_freq_points[pidx + 2] = (float) x;
-            this.m_freq_points[pidx + 1] = (float) this.height;
-            this.m_freq_points[pidx + 3] = (float) (this.height - y);
+          if (!freq_interp || last_x < 0 || x <= next_x) {
+            m_freq_points[pidx] = (float) x;
+            m_freq_points[pidx + 2] = (float) x;
+            m_freq_points[pidx + 1] = (float) height;
+            m_freq_points[pidx + 3] = (float) (height - y);
             pidx += 4;
           } else {
             if (loops >= 8) {
               loops /= 2;
             }
             for (int ctr = 0; ctr < loops; ctr++) {
-              this.m_freq_points[pidx] = (float) (next_x + ctr);
-              this.m_freq_points[pidx + 2] = (float) (next_x + ctr);
-              this.m_freq_points[pidx + 1] = (float) this.height;
-              this.m_freq_points[pidx + 3] = (float) (this.height - last_y);
+              m_freq_points[pidx] = (float) (next_x + ctr);
+              m_freq_points[pidx + 2] = (float) (next_x + ctr);
+              m_freq_points[pidx + 1] = (float) height;
+              m_freq_points[pidx + 3] = (float) (height - last_y);
               pidx += 4;
             }
           }
           last_x = x;
           last_y = y;
-        } else if (this.freq_avg_shared) {
+        } else if (freq_avg_shared) {
           cnt_y++;
-          this.m_freq_points[(pidx - 4) + 3] = (float) (this.height - ((y + last_y) / cnt_y));
+          m_freq_points[(pidx - 4) + 3] = (float) (height - ((y + last_y) / cnt_y));
         }
       }
-      canvas.drawLines(this.m_freq_points, this.freq_paint);
+      canvas.drawLines(m_freq_points, freq_paint);
     } catch (Throwable e) {
-      int i2 = this.num_throwable;
-      this.num_throwable = i2 + 1;
+      int i2 = num_throwable;
+      num_throwable = i2 + 1;
       if (i2 < 10) {
         com_uti.loge("Throwable: " + e);
         e.printStackTrace();
